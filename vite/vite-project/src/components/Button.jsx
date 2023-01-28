5a21b43ac3406fe46dc20764928aa32e0c1aa1bb
DEVair Martins <devair.martins@gmail.com>
2023-01-27 21:18:27 -0300

    mais cores

---
+++ vite/vite-project/src/components/Button.jsx
@@ -0,0 +1,13 @@
+function Button({texto, color}){
+    const cores={
+        "Verde":"green",
+        "Preto":"black"
+    }
+    return(
+        <button onClick={(e)=>{
+            document.querySelector('body').style.backgroundColor=color;
+            e.preventDefault()}}>
+        {texto}</button>
+    )
+}
+export default Button;
--- vite/vite-project/src/components/Form.jsx
+++ vite/vite-project/src/components/Form.jsx
@@ -1,3 +1,5 @@
+import Button from "./Button";
+
 function Form(){
     return(
         <form>
@@ -9,7 +11,10 @@ function Form(){
             </label><br />
             <label>Mensagem:
             <textarea cols="40" rows="5"></textarea>
-            </label>
+            </label><br />
+            <Button texto="Prop Enviar" color="green"></Button>
+            <Button texto="Prop Enviar" color="black"></Button>
+            <Button texto="Prop Enviar" color="red"></Button>
         </form>
     )
 }
